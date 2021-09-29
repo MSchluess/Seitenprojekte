256912c9bf6d09ff7e51bf33495d74cfb47cc7e0
MSchluess <89774043+MSchluess@users.noreply.github.com>
2021-09-29 13:37:02 +0200

    finished numGuesser

--- numGuess.js
+++ numGuess.js
@@ -1,19 +1,56 @@
 function rangeDeclared() {
+    const br = document.createElement('br');
     var lower = document.getElementById('von').value;
     var upper = document.getElementById('bis').value;
-
+    //check if the input is valid else give an error message
     if (lower == null || upper == null || lower >= upper) {
         alert('ERROR: Field empty or invalid numbers entered!');
         return;
     }
-
+    //disable the inputs so they cant be changed afterwards
     document.getElementById('von').disabled = true;
     document.getElementById('bis').disabled = true;
-    document.getElementById('start1').disabled = true;
-
-    var result = Math.floor(Math.random() * upper - lower) + lower;
-
+    document.getElementById('start1').hidden = true;
+    //generate the random number
+    var result = Math.floor(Math.random() * (upper - lower)) + parseInt(lower);
+    //create a hidden label so we can access it later
     var hiddenResultLabel = document.createElement('LABEL');
+    hiddenResultLabel.hidden = true;
     hiddenResultLabel.innerHTML = result;
+    hiddenResultLabel.setAttribute('id', 'hideRes');
     document.body.appendChild(hiddenResultLabel);
+    //create the new input field 
+    var guessField = document.createElement('INPUT');
+    guessField.setAttribute('id', 'guessField');
+    guessField.setAttribute('type', 'number');
+    document.getElementById('div').appendChild(br);
+    document.getElementById('div').appendChild(br);
+    document.getElementById('div').appendChild(guessField);
+    //create solution button
+    var guessButton = document.createElement('BUTTON');
+    guessButton.innerText = 'Rate!';
+    guessButton.setAttribute('onclick', 'solve()');
+    document.getElementById('div').appendChild(guessButton);
+}
+
+function solve() {
+    //pull valueable variables and constants
+    const br = document.createElement('br');
+    var lower = document.getElementById('von').value;
+    var upper = document.getElementById('bis').value;
+    var ans = document.getElementById('guessField').value;
+    const sol = document.getElementById('hideRes').innerHTML;
+    //check if the input is valid else give an error message
+    if (lower > ans || ans > upper) {
+        alert('Guessed Number should be within parameters.')
+        return;
+    }
+    //check wheater the given answere and give fitting output
+    document.getElementById('div').appendChild(br);
+    document.getElementById('div').appendChild(br);
+    document.getElementById('div').appendChild(br);
+    document.getElementById('div').appendChild(br);
+    var rightLabel = document.createElement('LABEL');
+    rightLabel.innerHTML = (ans == sol) ? "Korrekt!":"Falsch!";
+    document.getElementById('div').appendChild(rightLabel);
 }
